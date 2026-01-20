bad1e1c7bab33bc3879033ea9e14990da94a9b57
Maicon Daniel <maicon.daniel.lucas.pires@gmail.com>
2026-01-20 11:10:22 -0300

    Ajustando dominio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/purpurasec/', // Nome exato do seu repositório
+  base: '/', // Nome exato do seu repositório
 })
